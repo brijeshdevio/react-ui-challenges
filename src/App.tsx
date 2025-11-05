f5526bff4417a01cede18729eaaa45bfc512caf6
Brijesh <softbrijeshio@gmail.com>
2025-11-05 22:00:27 +0530

    feat: iOS Popup Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "@/components/layout";
-import { Home } from "@/pages";
+import { Home, IosPopup } from "@/pages";
 import "@/App.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/ios-popup" element={<IosPopup />} />
         </Routes>
       </BrowserRouter>
     </>
