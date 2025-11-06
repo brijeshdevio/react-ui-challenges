94853499a619951ec51dfa21ffdd8c66accce088
Brijesh <softbrijeshio@gmail.com>
2025-11-06 19:55:37 +0530

    feat(footer): add footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="w-full bg-zinc-900 border-t border-zinc-700">
+      <div className="text-center py-3 px-4 text-zinc-200 text-sm">
+        <p>Built with ❤️ using React + Tailwind CSS</p>
+      </div>
+    </footer>
+  );
+}
--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,13 +1,14 @@
-import { Navbar } from "@/components";
 import { Outlet } from "react-router-dom";
+import { Footer, Navbar } from "@/components";
 
 export function BaseLayout() {
   return (
     <>
       <Navbar />
-      <main className="w-full sm:w-[90%] h-[calc(100vh-53px)] mx-auto flex flex-col gap-5 px-3 py-10">
+      <main className="w-full sm:w-[90%] h-[calc(100vh-53px)] overflow-y-scroll mx-auto flex flex-col gap-5 px-3 py-10">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
